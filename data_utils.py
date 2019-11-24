1f301bacce5442ab878fd9a9c09932e0cd19ca4d
Larix-Shen <32137017+Larix@users.noreply.github.com>
2019-11-25 00:31:33 +0800

    data processing

--- data_utils.py
+++ data_utils.py
@@ -1,3 +1,5 @@
+__author__ = 'Larix-Shen'
+
 import sys, os
 import time
 import math
@@ -5,36 +7,113 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
 
 from datetime import datetime
+from statsmodels.tsa.stattools import adfuller
 
-df = pd.read_csv('data\\raw_data.csv')  
+def load_raw_data_to_process():
+    df = pd.read_csv('data\\raw_data.csv')  
+    # pre-processing
+    df = df.iloc[1:, :7]
+    df.rename(columns={df.columns[0]: "date", df.columns[1]: "clear_peak", df.columns[2]: "peak", 
+                        df.columns[3]: "operating_reserve", df.columns[4]: "p_operating_reserve", 
+                        df.columns[5]: "industry_usage", df.columns[6]: "house_usage"}, inplace = True)
+
+    df['date'] = pd.to_datetime(df['date'], format='%Y%m%d')
+    df = df.set_index('date')
+    df['weekday'] = pd.Series(df.index.dayofweek, index=df.index)
+    weekday_df = convert_to_weekday_form(df, 'peak')
+
+    #show_data_fig(df, weekday_df)
+    return weekday_df
+
+def auto_regression(ts_log_diff):
+    from statsmodels.tsa.ar_model import AR
+    from random import random
+    model = AR(ts_log_diff)
+    model_fit = model.fit()
 
 
-days = np.array(df.iloc[0:, 0])
-clear_peak = np.array(df.iloc[0:, 1]).astype(np.float)
-peak = np.array(df.iloc[0:, 2]).astype(np.float)
-operating_reserve = np.array(df.iloc[0:, 3]).astype(np.float)
-p_operating_reserve = np.array(df.iloc[0:, 4]).astype(np.float)
-industry_useage = np.array(df.iloc[0:, 5]).astype(np.float)
-house_useage = np.array(df.iloc[0:, 6]).astype(np.float)
+    plt.plot(ts_log_diff)
+    plt.plot(model_fit.fittedvalues, color='red')
+    plt.title('RSS: %.4f'% np.nansum((model_fit.fittedvalues-ts_log_diff)**2))
+    plt.show()
 
+def smoothing(weekday_df):
+    ts_log = np.log(weekday_df)
 
-dates = [datetime.strptime(format(day), '%Y%m%d').date() for day in days]
+    moving_avg = ts_log.rolling(4).mean()
+    ts_log_diff = ts_log - ts_log.shift()
+    ts_log_diff.dropna(inplace=True)
+    
+    ### check stationarity ###
+    #plt.plot(ts_log.peak_Mon)
+    #plt.plot(moving_avg.peak_Mon, color='red')
+    #plt.show()
+    #plt.plot(ts_log_diff.peak_Mon)
+    #plt.show()
+    #test_stationarity(ts_log_diff.peak_Mon)
+    return ts_log_diff
 
-'''
+def test_stationarity(timeseries):
+    #Determing rolling statistics
+    rolmean = timeseries.rolling(4).mean()
+    rolstd = timeseries.rolling(4).std()
+    #Plot rolling statistics:
+    orig = plt.plot(timeseries, color='blue',label='Original')
+    mean = plt.plot(rolmean, color='red', label='Rolling Mean')
+    std = plt.plot(rolstd, color='black', label = 'Rolling Std')
+    plt.legend(loc='best')
+    plt.title('Rolling Mean & Standard Deviation')
+    plt.show()
+    
+    #Perform Dickey-Fuller test:
+    print ('Results of Dickey-Fuller Test:')
+    dftest = adfuller(timeseries, autolag='AIC')
+    dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
+    for key,value in dftest[4].items():
+        dfoutput['Critical Value (%s)'%key] = value
+    print (dfoutput)
 
-fig_plot = plt.figure(figsize=(12,4),dpi=120)
-ax1 = fig_plot.add_subplot(1,1,1)
-ax1.plot(dates, peak, lw=1, c='g')
-ax1.set_title('peak (MW)')
-plt.show()
-'''
-print(np.median(peak))
+def get_train_test_set():
+    load_raw_data_to_process = load_raw_data_to_process()
+    train = df.sample(frac=0.8, random_state=200) #random state is a seed value
+    test = df.drop(train.index)
+
+# https://github.com/advaitsave/Introduction-to-Time-Series-forecasting-Python/blob/master/Time%20Series%20in%20Python.ipynb
+def show_data_fig(df, weekday_df):
+    show_specific_day_figure(df, 0)
+    show_weekday_figure(weekday_df)
+    show_peak_boxplot(weekday_df)
+
+def show_peak_boxplot(weekday_df):
+    #df.boxplot(column=list(df.columns))
+    sns.boxplot(data=weekday_df)
+    plt.show()
+
+def show_weekday_figure(weekday_df):
+    weekday_df.plot(title='Peak(MW)', figsize=(15, 6))
+    plt.show()   
+
+def show_specific_day_figure(df, day_number):
+    df = df.loc[df['weekday'] == day_number, ['peak']]
+    df.plot(title='Peak(MW)', figsize=(15, 6))
+    plt.show()   
+
+def convert_to_weekday_form(df, column_name):
+    weekday_data = {}
+    days = {0:"Mon", 1:"Tue", 2:"Wed", 3:"Thu", 4:"Fri", 5:"Sat", 6:"Sun"}
+    for i in range(7):
+        #week_df = df.loc[df['weekday'] == i, [column_name]]
+        weekday_data[column_name + "_" + str(days[i])] = df.loc[df['weekday'] == i][column_name].tolist()
+    weekday_df = pd.DataFrame.from_dict(weekday_data)
+    weekday_df.index.name = 'week'
+    return (weekday_df)
 
 #ref:https://towardsdatascience.com/5-ways-to-detect-outliers-that-every-data-scientist-should-know-python-code-70a54335a623
-def detect_outliers_data():
-	print(find_anomalies(peak))
+def detect_outliers_data(timeseries):
+	print(find_anomalies(timeseries))
 
 def find_anomalies(random_data):
     # Set upper and lower limit to 3 standard deviation
@@ -54,4 +133,6 @@ def find_anomalies(random_data):
     return anomalies
 
 if __name__ == '__main__':
-	detect_outliers_data()
+    weekday_df = load_raw_data_to_process()
+    weekday_df_log_diff = smoothing(weekday_df)
+    auto_regression(weekday_df_log_diff.peak_Mon)
